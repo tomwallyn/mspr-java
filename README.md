11f1a57e5757a5b3e8abef80a2497388c3f9f803
tomwallyn <tomwallyntel@gmail.com>
2022-04-18 16:41:10 +0200

    feat(access): generate .htaccess file to login

---
+++ README.md
@@ -0,0 +1,5 @@
+## User password 
+# (Encrypted with : https://www.web2generators.com/apache-tools/htpasswd-generator)
+
+cberthier:pmNd1ldFE7WTk
+sconnor:tdezhfe121MM2
--- src/main/java/com/mspr/utils/HTPasswdFileCreator.java
+++ src/main/java/com/mspr/utils/HTPasswdFileCreator.java
@@ -5,13 +5,18 @@ import java.util.ArrayList;
 
 public class HTPasswdFileCreator {
     private File htpasswdFile = new File("out/html/.htpasswd");
+    private File htaccessFile = new File("out/html/.htaccess");
 
 
     public void createHTPasswdFile(ArrayList<String> users) throws IOException {
         BufferedWriter bw = new BufferedWriter(new FileWriter(htpasswdFile));
+        BufferedWriter bwAccess = new BufferedWriter(new FileWriter(htaccessFile));
         ReaderJava readerJava = new ReaderJava();
+        bwAccess.write("AuthType Basic\nAuthName \"Authentification\"\nAuthUserFile " + "\"/var/www/html/.htpasswd\"" + "\nRequire valid-user");
+        bwAccess.newLine();
+        bwAccess.close();
         for (String user : users) {
-            bw.write(user+":$apr1$"+readerJava.getHtpasswd(user));
+            bw.write(user+":"+readerJava.getHtpasswd(user));
             bw.newLine();
         }
         bw.close();
